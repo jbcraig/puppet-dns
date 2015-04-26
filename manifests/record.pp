f2a874636a02cfcfc1d5301daf412218996ccb8f
Benny Cornelissen <mail@bennycornelissen.nl>
2015-04-26 14:28:39 +0200

    implemented static entries

--- manifests/record.pp
+++ manifests/record.pp
@@ -8,24 +8,24 @@ define dns::record (
 )
 {
 
+  $dnstype = upcase($type)
+
   if $label == undef {
     $label = $title
   }
-  if upcase($type) =~ /(CNAME|SRV|TXT)/ {
-    $type = upcase($type)
-    $line = "${label} IN ${type} ${target}"
+  if $dnstype =~ /(CNAME|SRV|TXT|A)/ {
+    $line = "${label} IN ${dnstype} ${target}"
   }
-  elsif upcase($type) == 'MX' {
-    $type = upcase($type)
-    $line = "${label} IN ${type} ${priority} ${target}"
+  elsif $dnstype == 'MX' {
+    $line = "${label} IN ${dnstype} ${priority} ${target}"
   }
   else {
     warning('Incorrect DNS record type specified')
   }
 
 
-  file_line { "static.${title}":
-    path    => "/etc/bind/${domain}.static",
+  file_line { "static.${domain}.${title}":
+    path    => "${::dns::zonefilepath}/db.${domain}.static",
     line    => $line,
     tag     => "dns-static-${domain}",
   }
